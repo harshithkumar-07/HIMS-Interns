8e7439a00b904ffc73bc6cbd28577653ba670a75
karthikr78677 <karthikr78677@gmail.com>
2026-02-23 11:08:28 +0530

    patient_complaint backend created

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express"
 import "dotenv/config.js"
 
 import patient_feedback from "./routes/feedback_Routes.js"
+
+import patient_complaints from "./routes/complaints.Routes.js"
 const app=express()
 app.use(express.json())
 const PORT=process.env.PORT || 3000
@@ -9,6 +11,7 @@ const PORT=process.env.PORT || 3000
 import con from "./db.js"
 
 app.use("/feedback",patient_feedback)
+app.use("/complaints",patient_complaints)
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`)
 })
